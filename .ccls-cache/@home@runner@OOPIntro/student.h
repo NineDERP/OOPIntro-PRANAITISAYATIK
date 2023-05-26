cbc1b71dac674e768a07c46ce082f7e1969b6780
NineDERP <132637581+NineDERP@users.noreply.github.com>
2023-05-26 04:55:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@OOPIntro/student.h
@@ -0,0 +1,6 @@
+class student{
+private:
+
+public:
+
+};
---
+++ student.h
@@ -0,0 +1,29 @@
+class student{
+private: 
+  long id;
+  string name;
+  int age;
+public:
+    void set_name(string="Yme");
+     void print_name();
+      void set_age(int);
+      int get_age();
+};
+
+void student::set_name(string n){
+  name=n;
+  
+}
+
+void student::print_name(){
+  cout<<"Name:"<<name<<endl;
+}
+
+void student::set_age(int n){
+  age=n;
+}
+
+int student::get_age(){
+  return age;
+}
+  
